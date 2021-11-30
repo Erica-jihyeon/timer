15c7097e5a40513872f8fc2829f61fc1cf2db8ca
Erica-jihyeon <jihyeon20@gmail.com>
2021-11-30 23:20:48 +0000

    timer1

---
+++ timer1.js
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2).sort((a,b) => a-b).map(e => {
+  if (e == Number(e)) {
+    return Number(e);
+  } else {
+    return e;
+  }
+});
+//console.log(args);
+
+if (args.length === 0) {
+  console.log('no input');
+} else if (args.some(e => e < 0)) {
+  console.log('negative time');
+} else if (args.some(e => typeof e !== 'number')) {
+  console.log('not a number')
+} 
+else {
+  for (const time of args) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, time*1000);
+  }
+}
